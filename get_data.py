c2c7dac6bf631bb1e8298d0e65cad4ae5a1f9dfd
Andrew Berg <berg@squizzer.io>
2020-11-16 12:07:55 +0200

    Corrected path to save data

--- get_data.py
+++ get_data.py
@@ -49,7 +49,7 @@ for d in dates:
     if df_date.empty:
         print('No data for this date: {}'.format(d))
         continue
-    df_date.to_csv(cwd + d + '.csv')
+    df_date.to_csv(cwd + '/data/imbalances/' + d + '.csv')
     print('Date {} saved'.format(d))
 
 end = time.time()
